af8e43427063b3e7b1f2169c5b58b28175c9e8ab
Gene Lee <gwlee244@gmail.com>
2020-09-29 18:22:55 -0400

    updated header color and how to donate

--- src/components/main.js
+++ src/components/main.js
@@ -22,12 +22,16 @@ class Main extends Component {
             <h3>How do I donate?</h3>
             <div> 
              <p>
-             You can either contribute an amount or get others to sponsor you. Donate whatever amount makes sense for you. For example, if you think you will exercise 30 min a day for 5 days, that's 150 minutes. If you raised/contributed $1/minute you would donate $150 for financial aid for students in need. $.50/minute would be $75 and so on. 
-             Contribute here<form action="https://Simplecheckout.authorize.net/payment/CatalogPayment.aspx" method="post" name="PrePage"><input name="LinkId" type="hidden" value="1ba5bba0-d140-48bc-a55d-24e39a8bd564" /> <button className="donate-link" type="submit" >DONATE</button></form></p>
+              To participate, you may contribute a pre-determined amount or get others to sponsor you. 
+              Sponsors may make a onetime donation OR may sponsor you based on the number of minutes you exercise (ex. 30 min a day for 5 days = 150 minutes. If someone sponsors you $1/minute that equals $150 for financial aid).  
+              Send your friends an email and share this page, suggest they support financial aid by sponsoring you or your team! You could even invite them to join! (They can participate and donate to the event.)
+
+              Your donation amount is not important, participating and having fun is! Contribute what is comfortable for you.You can either contribute an amount or get others to sponsor you. Donate whatever amount makes sense for you. For example, if you think you will exercise 30 min a day for 5 days, that's 150 minutes. If you raised/contributed $1/minute you would donate $150 for financial aid for students in need. $.50/minute would be $75 and so on. 
+              Contribute here<form action="https://Simplecheckout.authorize.net/payment/CatalogPayment.aspx" method="post" name="PrePage"><input name="LinkId" type="hidden" value="1ba5bba0-d140-48bc-a55d-24e39a8bd564" /> <button className="donate-link" type="submit" >DONATE</button></form></p>
             </div>
             <h3>Sounds fun, When is it?</h3>
             <div>
-             <p>The event starts on Monday and ends on Friday, November 9-13, 2020.  
+             <p>The event starts on Monday and ends on Sunday, November 9-15, 2020.  
                Because it is virtual, you can fit it in whenever you have time during those days.
                Thank you for your participation!</p>
             </div>
