682a00bc1b6f5fe212ca683a87a79e7c51fcd22a
Gene Lee <gwlee244@gmail.com>
2020-09-29 21:32:32 -0400

    added bullet points

--- src/components/main.js
+++ src/components/main.js
@@ -22,11 +22,11 @@ class Main extends Component {
             <h3>How do I donate?</h3>
             <div> 
              <p>
-              To participate, you may contribute a pre-determined amount or get others to sponsor you. 
-              Sponsors may make a onetime donation OR may sponsor you based on the number of minutes you exercise (ex. 30 min a day for 5 days = 150 minutes. If someone sponsors you $1/minute that equals $150 for financial aid).  
-              Send your friends an email and share this page, suggest they support financial aid by sponsoring you or your team! You could even invite them to join! (They can participate and donate to the event.)
+             &#8226; To participate, you may contribute a pre-determined amount or get others to sponsor you. <br></br>
+             &#8226; Sponsors may make a onetime donation OR may sponsor you based on the number of minutes you exercise (ex. 30 min a day for 5 days = 150 minutes. If someone sponsors you $1/minute that equals $150 for financial aid). <br></br>
+             &#8226; Send your friends an email and share this page, suggest they support financial aid by sponsoring you or your team! You could even invite them to join! (They can participate and donate to the event.) <br></br>
 
-              Your donation amount is not important, participating and having fun is! Contribute what is comfortable for you.You can either contribute an amount or get others to sponsor you. Donate whatever amount makes sense for you. For example, if you think you will exercise 30 min a day for 5 days, that's 150 minutes. If you raised/contributed $1/minute you would donate $150 for financial aid for students in need. $.50/minute would be $75 and so on. 
+             &#8226; <b>Your donation amount is not important, participating and having fun is! Contribute what is comfortable for you.</b> <br></br>
               Contribute here<form action="https://Simplecheckout.authorize.net/payment/CatalogPayment.aspx" method="post" name="PrePage"><input name="LinkId" type="hidden" value="1ba5bba0-d140-48bc-a55d-24e39a8bd564" /> <button className="donate-link" type="submit" >DONATE</button></form></p>
             </div>
             <h3>Sounds fun, When is it?</h3>
